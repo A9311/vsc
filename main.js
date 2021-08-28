3e6c86177a95f7e654c054189274b14bfcd92bed
A9311 <87259367+A9311@users.noreply.github.com>
2021-08-28 17:12:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+var first_name = "Aadya";
+var last_name = "Singhal";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 2;
+
+var add = a + b ;
+console.log(add);
+
+var sub = a - b ;
+console.log(sub);
+
+var multiple = a * b ;
+console.log(multiple);
+
+var divide = a / b ;
+console.log(divide);
